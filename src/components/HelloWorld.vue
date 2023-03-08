c47f5dbb78b0bb7d73a2999cb9463189f0e7a8b4
drunkzhuo <2745914927@qq.com>
2023-03-08 22:13:56 +0800

    修改一些title

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      这是一个「灵活」且「优雅」的 UI 库 ～
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
---
+++ src/views/DocView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="doc">
+    <header>
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </header>
+    <main>
+      <aside></aside>
+      <div class="content"></div>
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+@media (min-width: 1024px) {
+  .doc {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
