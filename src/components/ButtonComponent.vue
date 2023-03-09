f61b717911185bfe123380009609d570973a3b54
drunkzhuo <2745914927@qq.com>
2023-03-09 15:00:38 +0800

    创建button组件

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import Button from '../lib/Button.vue'
+const onClick = () => {
+    console.log(1);
+    
+}
 </script>
 <template>
     <div>
-        button
+        <Button @click="onClick">提交</Button>
     </div>
 </template>
---
+++ src/lib/Button.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+// 使用普通的 <script> 来声明选项
+export default {
+  inheritAttrs: false
+}
+</script>
+<script setup lang="ts">
+import { useAttrs } from 'vue'
+const attrs = useAttrs()
+// 可以使用 {size, ...rest} = useAttrs()将属性分开
+</script>
+<template>
+    <div class="button">
+        <button v-bind="attrs">
+            <slot />
+        </button>
+    </div>
+</template>
