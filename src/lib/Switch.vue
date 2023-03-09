2f274f35b5ae6ddcd3ff1e03cfbfeb051ce78c30
drunkzhuo <2745914927@qq.com>
2023-03-09 14:32:10 +0800

    switch组件添加样式

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -16,34 +16,52 @@ const toggle = () => {
 }
 </script>
 <template>
-    <button 
-    class="btn"
-        :class="{checked}"
-        @click="toggle"
-    >
-        <span></span>
-    </button>
-
+    <div class="switch">
+        <button 
+        class="btn"
+            :class="{checked}"
+            @click="toggle"
+        >
+            <span></span>
+        </button>
+    </div>
 </template>
 
 <style lang="scss">
+.switch{
+    z-index: 1;
+    display: inline-block;
+    height: $button-height;
+    width: $button-width;
+}
+
 .btn{
+    cursor: pointer;
     height: $button-height;
     width: $button-width;
     border: none;
-    border-radius: $button-height/2;
+    box-shadow:1px 1px 4px $shadow-color;
+    border-radius: 14px;
     position: relative;
+    transition: all .4s ease;
     >span{
+        cursor: pointer;
+        background-color: $default-background;
         position: absolute;
-        top: 0;
-        left: 1px;
-        width: $button-height;
-        height: $button-height;
-        background-color: #fff;
-        border-radius: $button-height/2;
+        top: 4px;
+        left: 4px;
+        width: $cilcle-radius;
+        height: $cilcle-radius;
+        border-radius: 50%;
+        transition: all .4s ease;
     }
 }
 .checked{
-    background-color: red;
+    background-image: linear-gradient(to right, $activated-background-left,$activated-background-right);
+    >span{
+        transform: translateX(calc($button-width - $button-height - 2px));
+        background-color: $white-plus;
+
+    }
 }
 </style>
