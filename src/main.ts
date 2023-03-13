a47e2e220d30e64fcc2ddd29336c0fdef8689b97
drunkzhuo <2745914927@qq.com>
2023-03-13 21:31:42 +0800

    引入github样式

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import '//at.alicdn.com/t/c/font_3942763_bk7brj26xuv.js'
 import './assets/main.css'
 import './lib/space.scss'
 
+import 'github-markdown-css'
+
 const app = createApp(App)
 
 app.use(router)
