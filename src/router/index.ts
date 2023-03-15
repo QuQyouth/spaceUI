5cba3b849f9b7301b57a4521a9581907f8bfe860
drunkzhuo <2745914927@qq.com>
2023-03-15 13:42:28 +0800

    test 路径

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,7 +12,7 @@ import Checkbox from '../components/CheckboxComponent.vue'
 import Intro from '@/markdown/intro.md'
 const router = createRouter({
   // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
